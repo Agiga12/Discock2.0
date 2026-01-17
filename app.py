08d2227a16a315c7af6a4968af880f6aebf4fbda
Даниил <80783939+Agiga12@users.noreply.github.com>
2026-01-18 00:26:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Хранилище пользователей: {sid: {'nickname': str, 'room': str}}
+# Хранилище: {sid: {'nickname': str, 'avatar': str, 'room': str}}
 users = {}
 
 @app.route('/')
@@ -17,26 +17,33 @@ def index():
 def on_join(data):
     room = data['room']
     nickname = data.get('nickname', 'Аноним')
+    avatar = data.get('avatar', 'https://via.placeholder.com/150')
     
     join_room(room)
-    users[request.sid] = {'nickname': nickname, 'room': room}
+    users[request.sid] = {'nickname': nickname, 'avatar': avatar, 'room': room}
     
-    # Отправляем обновленный список участников всем в комнате
-    room_users = [u['nickname'] for sid, u in users.items() if u['room'] == room]
+    # Рассылаем список участников с аватарками
+    room_users = [{'nickname': u['nickname'], 'avatar': u['avatar']} for sid, u in users.items() if u['room'] == room]
     emit('update-user-list', {'users': room_users, 'roomName': room}, room=room)
     
-    # Сигнал для WebRTC
     emit('user-connected', {'sid': request.sid}, room=room, include_self=False)
+
+@socketio.on('leave_room_custom')
+def on_leave(data):
+    room = data['room']
+    leave_room(room)
+    if request.sid in users:
+        del users[request.sid]
+    
+    room_users = [{'nickname': u['nickname'], 'avatar': u['avatar']} for sid, u in users.items() if u['room'] == room]
+    emit('update-user-list', {'users': room_users, 'roomName': room}, room=room)
 
 @socketio.on('disconnect')
 def on_disconnect():
     if request.sid in users:
-        user_data = users[request.sid]
-        room = user_data['room']
+        room = users[request.sid]['room']
         del users[request.sid]
-        
-        # Обновляем список для оставшихся
-        room_users = [u['nickname'] for sid, u in users.items() if u['room'] == room]
+        room_users = [{'nickname': u['nickname'], 'avatar': u['avatar']} for sid, u in users.items() if u['room'] == room]
         emit('update-user-list', {'users': room_users, 'roomName': room}, room=room)
 
 @socketio.on('signal')
